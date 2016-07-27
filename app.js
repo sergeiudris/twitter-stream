af307744e46db48d270e7bd60979b58beb102e3e
seeris <sergei.udris@gmail.com>
2016-07-27 12:04:06 +0300

    http socket

--- app.js
+++ app.js
@@ -1,14 +1,64 @@
 const http         = require('http'),
+      https         = require('http'),
       fs           = require('fs'),
       path         = require('path'),
       contentTypes = require('./utils/content-types'),
       sysInfo      = require('./utils/sys-info'),
       env          = process.env;
 
+const PORT = process.env.OPENSHIFT_NODEJS_PORT || 8000;
+
 //const Twit = require('twit')
-var sserver = require('https').createServer();
-var io = require('socket.io')(sserver);
-const PORT = process.env.OPENSHIFT_NODEJS_PORT || 8443;
+// var sserver = require('https').createServer({},);
+// var io = require('socket.io')(sserver);
+// const PORT = process.env.OPENSHIFT_NODEJS_PORT || 8443;
+// io.on('connection', function (socket) {
+//     console.log("a user connected");
+
+//     socket.on('disconnect', function (e) {
+//         console.log("disconnect");
+//     });
+//     socket.on('chat message', function (o) {
+//         socket.broadcast.emit("chat message", o);
+//     });
+// });
+// sserver.listen(PORT, function () {
+//     console.log('>app is running on port ' + PORT);
+// }) 
+
+let server = http.createServer(function (req, res) {
+  let url = req.url;
+  if (url == '/') {
+    url += 'index.html';
+  }
+
+  // IMPORTANT: Your application HAS to respond to GET /health with status 200
+  //            for OpenShift health monitoring
+
+  if (url == '/health') {
+    res.writeHead(200);
+    res.end();
+  } else if (url == '/info/gen' || url == '/info/poll') {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Cache-Control', 'no-cache, no-store');
+    res.end(JSON.stringify(sysInfo[url.slice(6)]()));
+  } else {
+    fs.readFile('./static' + url, function (err, data) {
+      if (err) {
+        res.writeHead(404);
+        res.end('Not found');
+      } else {
+        let ext = path.extname(url).slice(1);
+        res.setHeader('Content-Type', contentTypes[ext]);
+        if (ext === 'html') {
+          res.setHeader('Cache-Control', 'no-cache, no-store');
+        }
+        res.end(data);
+      }
+    });
+  }
+});
+var io = require('socket.io')(server);
 io.on('connection', function (socket) {
     console.log("a user connected");
 
@@ -19,43 +69,7 @@ io.on('connection', function (socket) {
         socket.broadcast.emit("chat message", o);
     });
 });
-sserver.listen(PORT, function () {
-    console.log('>app is running on port ' + PORT);
-}) 
 
-// let server = http.createServer(function (req, res) {
-//   let url = req.url;
-//   if (url == '/') {
-//     url += 'index.html';
-//   }
-
-//   // IMPORTANT: Your application HAS to respond to GET /health with status 200
-//   //            for OpenShift health monitoring
-
-//   if (url == '/health') {
-//     res.writeHead(200);
-//     res.end();
-//   } else if (url == '/info/gen' || url == '/info/poll') {
-//     res.setHeader('Content-Type', 'application/json');
-//     res.setHeader('Cache-Control', 'no-cache, no-store');
-//     res.end(JSON.stringify(sysInfo[url.slice(6)]()));
-//   } else {
-//     fs.readFile('./static' + url, function (err, data) {
-//       if (err) {
-//         res.writeHead(404);
-//         res.end('Not found');
-//       } else {
-//         let ext = path.extname(url).slice(1);
-//         res.setHeader('Content-Type', contentTypes[ext]);
-//         if (ext === 'html') {
-//           res.setHeader('Cache-Control', 'no-cache, no-store');
-//         }
-//         res.end(data);
-//       }
-//     });
-//   }
-// });
-
-// server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
-//   console.log(`Application worker ${process.pid} started...`);
-// });
+server.listen(PORT, env.OPENSHIFT_NODEJS_IP  || 'localhost', function () {
+  console.log(`Application worker ${process.pid} started...`);
+});
